db7a172056d3f4defc247f973b9bf0998e85592c
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2024-04-17 20:47:04 +0200

    refactor(stack): add stack capacity, isFull & change

--- data-structures/stack/stack.ts
+++ data-structures/stack/stack.ts
@@ -1,78 +1,68 @@
-/**
- * Represents a generic stack data structure.
- * @template T The type of elements held in the stack.
- */
 export default class Stack<T> {
-  /**
-   * Internal array to hold the stack's elements.
-   * @private
-   */
   private elements: T[];
+  private capacity: number;
 
-  /**
-   * Initializes a new instance of the Stack class, creating an empty stack.
-   */
-  constructor() {
-    this.elements = [];
+  constructor(maxSize: number = 100) {
+    this.capacity = maxSize;
+    this.elements = new Array<T>(maxSize);
   }
 
-  /**
-   * Adds an element to the top of the stack.
-   * @param {T} element The element to be added to the stack.
-   * @returns {number} The new size of the stack after the element is added.
-   */
   push(element: T): number {
+    if (this.isFull()) {
+      throw new Error(`OverflowError: cannot push to a full stack!`);
+    }
     return this.elements.push(element);
   }
 
-  /**
-   * Removes the element from the top of the stack and returns it.
-   * If the stack is empty, returns undefined.
-   * @returns {T | undefined} The element removed from the top of the stack, or undefined if the stack was empty.
-   */
   pop(): T | undefined {
+    if (this.isEmpty()) {
+      throw new Error("UnderflowError: cannot pop from an empty stack!");
+    }
     return this.elements.pop();
   }
 
-  /**
-   * Returns the element at the top of the stack without removing it.
-   * If the stack is empty, returns null.
-   * @returns {T | null} The element at the top of the stack or null if the stack is empty.
-   */
   peek(): T | null {
     if (this.isEmpty()) {
       return null;
     }
-    return this.elements[this.size() - 1];
+    return this.elements[this.count() - 1];
   }
 
-  /**
-   * Checks whether the stack is empty.
-   * @returns {boolean} True if the stack is empty, false otherwise.
-   */
   isEmpty(): boolean {
-    return this.size() === 0;
+    return this.count() === 0;
   }
 
-  /**
-   * Returns the number of elements in the stack.
-   * @returns {number} The number of elements currently in the stack.
-   */
-  size(): number {
+  isFull(): boolean {
+    return this.elements.length === this.capacity;
+  }
+
+  count(): number {
     return this.elements.length;
   }
 
-  /**
-   * Removes all elements from the stack, effectively emptying it.
-   */
   clear(): void {
     this.elements = [];
   }
 
-  /**
-   * Prints the elements of the stack to the console.
-   */
-  print(): void {
+  change(index: number, element: T) {
+    if (this.isEmpty()) {
+      throw new Error(
+        "UnderflowError: cannot modify an element in an empty stack!"
+      );
+    }
+
+    if (index < 0 && index >= this.capacity) {
+      throw new RangeError(
+        `Invalid element position. Position should be within 0 & ${
+          this.capacity - 1
+        }`
+      );
+    }
+
+    this.elements[index] = element;
+  }
+
+  display(): void {
     console.log(this.elements.toString());
   }
 }
